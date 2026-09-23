abefaa88d040d64554df6368f58eb3f6a29a431c
zxc3974 <zxcn3974@gmail.com>
2026-09-22 22:34:02 -0500

    feat: add health check endpoint to the application

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,15 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+app.get('/api/v1/health', (req, res) => {
+
+  res.json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0",
+  })
+})
 
 
 export default app;
