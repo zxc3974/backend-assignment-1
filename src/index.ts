c6cafcc1964a59d30b9a1a2416031c042a58e017
zxc3974 <zxcn3974@gmail.com>
2026-09-22 21:35:41 -0500

    feat: add express server setup and update configuration files

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
